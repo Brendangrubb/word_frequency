233ff9162f44d82b98aa80d05320703c710b1f22
Brendan Grubb <brendangrubb@gmail.com>
2017-02-17 11:23:53 -0800

    add functionality to find all occurrences of word within a phrase regardless of case

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,22 +4,22 @@
         function countRepeats($input1, $input2)
         {
             $word_counter = 0;
-            // echo "before if: " . $word_counter;
-            if ($input1 == $input2) {
+            $lowercase_input1 = strtolower($input1);
+            $lowercase_input2 = strtolower($input2);
+
+            if ($lowercase_input1 == $lowercase_input2) {
                 ++ $word_counter;
-                // echo " after 1st if: " . $word_counter;
 
             }
             else
             {
-                $input2_word_split = explode(" ", $input2);
+                $input2_word_split = explode(" ", $lowercase_input2);
                 foreach ($input2_word_split as $input2_word)
                 {
-                    if ($input2_word == $input1)
+                    if ($input2_word == $lowercase_input1)
                     {
                         ++ $word_counter;
                     }
-                    // echo " after 2nd if: " . $word_counter;
 
                 }
             }
