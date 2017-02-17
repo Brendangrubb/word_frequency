9fd67ef85439edce5b7bf2e673796dbe45cdb508
Brendan Grubb <brendangrubb@gmail.com>
2017-02-17 10:54:59 -0800

    add test to recognize no occurrences of a word within a phrase

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -6,12 +6,23 @@
         function test_countRepeats_oneToOne()
         {
             $test_RepeatCounter = new RepeatCounter;
-            $input1 = shiny;
-            $input2 = shiny;
+            $input1 = "shiny";
+            $input2 = "shiny";
 
             $result = $test_RepeatCounter->countRepeats($input1, $input2);
 
             $this->assertEquals(1, $result);
+        }
+
+        function test_countRepeats_oneToPhraseNoMatch()
+        {
+            $test_RepeatCounter = new RepeatCounter;
+            $input1 = "shiny";
+            $input2 = "boots of leather";
+
+            $result = $test_RepeatCounter->countRepeats($input1, $input2);
+
+            $this->assertEquals(0, $result);
         }
     }
 ?>
