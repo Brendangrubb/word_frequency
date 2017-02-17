f7a065c6f70260f0240031bb14f079fbd6dfaf87
Brendan Grubb <brendangrubb@gmail.com>
2017-02-17 12:52:07 -0800

    add more to  array

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,7 +6,7 @@
             $word_counter = 0;
             $lowercase_input1 = strtolower($input1);
             $lowercase_input2 = strtolower($input2);
-            $punctuation = array("!", "?", ".");
+            $punctuation = array("!", "?", ".", ",", "-", ":", "'", '"', );
             $no_punctuation_input1 = str_replace($punctuation, " ", $lowercase_input1);
             $no_punctuation_input2 = str_replace($punctuation, " ", $lowercase_input2);
 
