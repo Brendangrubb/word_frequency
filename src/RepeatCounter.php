2f8a4b53ce452f9c78028a830467a7c4e8797f46
Brendan Grubb <brendangrubb@gmail.com>
2017-02-17 12:47:13 -0800

    add functionality to find all occurrences of word within a phrase regardless of punctuation

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,21 +6,22 @@
             $word_counter = 0;
             $lowercase_input1 = strtolower($input1);
             $lowercase_input2 = strtolower($input2);
+            $punctuation = array("!", "?", ".");
+            $no_punctuation_input1 = str_replace($punctuation, " ", $lowercase_input1);
+            $no_punctuation_input2 = str_replace($punctuation, " ", $lowercase_input2);
 
-            if ($lowercase_input1 == $lowercase_input2) {
+            if ($lowercase_input1 == $no_punctuation_input2) {
                 ++ $word_counter;
-
             }
             else
             {
-                $input2_word_split = explode(" ", $lowercase_input2);
+                $input2_word_split = explode(" ", $no_punctuation_input2);
                 foreach ($input2_word_split as $input2_word)
                 {
                     if ($input2_word == $lowercase_input1)
                     {
                         ++ $word_counter;
                     }
-
                 }
             }
 
