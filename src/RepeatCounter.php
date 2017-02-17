852db0f4801834484ca5d0081af9ea4e11ddc3ee
Brendan Grubb <brendangrubb@gmail.com>
2017-02-17 11:06:49 -0800

    add functionality to recognize no occurrences of a word within a phrase

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -8,7 +8,18 @@
             if ($input1 == $input2) {
                 ++ $word_counter;
             }
-            
+            else
+            {
+                $input2_word_split = explode(" ", $input2);
+                foreach ($input2_word_split as $input2_word)
+                {
+                    if ($input2_word == $input1)
+                    {
+                        ++ $word_counter;
+                    }
+                }
+            }
+
             return $word_counter;
         }
     }
