e13a22188e6275db60735703dfc9d1a9ab36df01
Brendan Grubb <brendangrubb@gmail.com>
2017-02-17 13:13:34 -0800

    populate index.php and app.php, link twig

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,11 +6,11 @@
             $word_counter = 0;
             $lowercase_input1 = strtolower($input1);
             $lowercase_input2 = strtolower($input2);
-            $punctuation = array("!", "?", ".", ",", "-", ":", "'", '"', );
+            $punctuation = array("!", "?", ".", ",", "-", ":", "'", '"');
             $no_punctuation_input1 = str_replace($punctuation, " ", $lowercase_input1);
             $no_punctuation_input2 = str_replace($punctuation, " ", $lowercase_input2);
 
-            if ($lowercase_input1 == $no_punctuation_input2) {
+            if ($no_punctuation_input1 == $no_punctuation_input2) {
                 ++ $word_counter;
             }
             else
