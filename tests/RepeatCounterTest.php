c1558c8ce80057f2bb7f6acb4d338a4c26bf96b3
Brendan Grubb <brendangrubb@gmail.com>
2017-02-17 10:43:46 -0800

    add test to recognize one word and another word as the same

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -3,6 +3,15 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
+        function test_countRepeats_oneToOne()
+        {
+            $test_RepeatCounter = new RepeatCounter;
+            $input1 = shiny;
+            $input2 = shiny;
 
+            $result = $test_RepeatCounter->countRepeats($input1, $input2);
+
+            $this->assertEquals(1, $result);
+        }
     }
 ?>
